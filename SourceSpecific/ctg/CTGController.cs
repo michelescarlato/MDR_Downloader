755f119995d408bee37ed971969e31c3d0b5ca88
Michele Scarlato <michele.scarlato@gmail.com>
2024-10-01 15:06:22 +0200

    [ctg] fixing output directory (a separator is missing).

--- SourceSpecific/ctg/CTGController.cs
+++ SourceSpecific/ctg/CTGController.cs
@@ -393,7 +393,7 @@ class CTG_Controller : IDLController
 
         // Then write out study file as indented json
 
-        string folder_path = file_base + file_folder;
+        string folder_path = file_base + " / " + file_folder;
         if (!Directory.Exists(folder_path))
         {
             Directory.CreateDirectory(folder_path);
