3cb79e720209334eca4a612f8abba67a6f5f17f0
Michele <michele.scarlato@gmail.com>
2026-01-01 13:33:32 +0100

    Adding pubmed as a file_base in case the file_base var is empty.

--- SourceSpecific/pubmed/PubMedController.cs
+++ SourceSpecific/pubmed/PubMedController.cs
@@ -466,6 +466,12 @@ public class PubMed_Controller : IDLController
 
     public async Task FetchPubMedRecordsAsync(string fetch_URL, DownloadResult res, int dl_id, string file_base)
     {
+        // Default folder if caller passed nothing
+        if (string.IsNullOrWhiteSpace(file_base))
+        {
+            _loggingHelper.LogLine($"[PUBMED] file_base is empty for dl_id={dl_id}. Defaulting to 'pubmed'.");
+            file_base = "pubmed";
+        }
         string? responseBody = await ch.GetAPIResponseWithRetriesAsync(fetch_URL, 1000, fetch_URL);
         if (responseBody is null)
         {
